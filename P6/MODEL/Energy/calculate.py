3381deade6615f5e1636e83de5da704d7543e215
Rikku1 <23-01987@g.batstate-u.edu.ph>
2024-12-07 15:22:28 +0800

    calcu PE and KE

--- P6/MODEL/Energy/calculate.py
+++ P6/MODEL/Energy/calculate.py
@@ -4,35 +4,34 @@ current_dir = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, os.path.join(current_dir, "Energy"))
 from tkinter import messagebox
 
-class CalculateEnergy:
+class CalculatePotentialEnergy:
     def __init__(self, root, result_label):
         self.root = root
         self.result_label = result_label
     
-    def calculate(self, work_entry, time_entry):
+    def calculate(self, height, mass):
         try:
-            work = float(work_entry.get())
-            time = float(time_entry.get())
-            if time == 0:
-                raise ZeroDivisionError("Time cannot be zero.")
-            power = work / time
-            self.result_label.config(text=f"{power:.2f} W")  # Update result label
+            height = float(height.get())
+            mass = float(mass.get())
+            
+            PE = mass  * 9.81 * height
+            self.result_label.config(text=f"{PE:.2f} J")  # Update result label
         except ValueError:
             messagebox.showerror("Invalid Input", "Please enter numeric values for Work and Time.")
         except ZeroDivisionError as e:
             messagebox.showerror("Error", str(e))
 
-class CalculateWork:
+class CalculateKineticEnergy:
     def __init__(self, root, result_label):
         self.root = root
         self.result_label = result_label 
     
-    def calculate(self, power_entry, time_entry):
+    def calculate(self, mass, velocity):
         try:
-            power = float(power_entry.get())
-            time = float(time_entry.get())
-            work = power * time
-            self.result_label.config(text=f"{work:.2f} J")  # Update result label
+            mass = float(mass.get())
+            velocity = float(velocity.get())
+            KE = 0.5 * mass * (velocity**2)
+            self.result_label.config(text=f"{KE:.2f} J")  # Update result label
         except ValueError:
             messagebox.showerror("Invalid Input", "Please enter numeric values for Power and Time.")
 
