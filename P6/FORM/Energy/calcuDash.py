5a81ffc32652e5dbf784c937fdbf0bf267f4cd70
Gertrude Kristine Agena <saucegettie@gmail.com>
2024-12-08 15:11:31 +0800

    energy tabs ui | done

--- P6/FORM/Energy/calcuDash.py
+++ P6/FORM/Energy/calcuDash.py
@@ -51,6 +51,12 @@ class CalcuDashboard():
             self.study = PhotoImage(file="AssetsEnergy/study.png")
             self.master = PhotoImage(file="AssetsEnergy/master.png")
             self.calcuSelected = PhotoImage(file="AssetsEnergy/calcuS.png")
+            self.powerTab = PhotoImage(file="UTILITY/powerr.png")
+            self.energyTab = PhotoImage(file="UTILITY/energyS.png")
+            self.workTab = PhotoImage(file="UTILITY/work.png")
+            self.powerHover = PhotoImage(file="UTILITY/powerS.png")
+            self.energyHover = PhotoImage(file="UTILITY/energyS.png")
+            self.workHover = PhotoImage(file="UTILITY/workS.png")
 
             # Create images on the canvas
             self.canvas_image = self.canvas.create_image(0, 0, anchor=NW, image=self.bg)
@@ -95,6 +101,45 @@ class CalcuDashboard():
             command=self.controller.open_calculatorKE
         )
         self.canvas.create_window(1085, 556, anchor=NW, window=calcuButton)
+
+        powerButton = Button(
+            root, image=self.powerTab,
+            borderwidth=0,
+            background="#ffffff",
+            activebackground="#ffffff",
+            cursor="hand2",
+            # command=self.controller.open_problemSet
+        )
+        # Bind hover effects to masterButton
+        powerButton.bind("<Enter>", lambda event: self.on_enter(event, powerButton, self.powerHover))
+        powerButton.bind("<Leave>", lambda event: self.on_leave(event, powerButton, self.powerTab))
+        self.powerButton_window = self.canvas.create_window(90, 230, anchor=NW, window=powerButton)
+
+        energyButton = Button(
+            root, image=self.energyTab,
+            borderwidth=0,
+            background="#ffffff",
+            activebackground="#ffffff",
+            cursor="hand2",
+            # command=self.controller.open_problemSet
+        )
+        # Bind hover effects to masterButton
+        energyButton.bind("<Enter>", lambda event: self.on_enter(event, energyButton, self.energyHover))
+        energyButton.bind("<Leave>", lambda event: self.on_leave(event, energyButton, self.energyTab))
+        self.energyButton_window = self.canvas.create_window(90, 310, anchor=NW, window=energyButton)
+
+        workButton = Button(
+            root, image=self.workTab,
+            borderwidth=0,
+            background="#ffffff",
+            activebackground="#ffffff",
+            cursor="hand2",
+            # command=self.controller.open_problemSet
+        )
+        # Bind hover effects to masterButton
+        workButton.bind("<Enter>", lambda event: self.on_enter(event, workButton, self.workHover))
+        workButton.bind("<Leave>", lambda event: self.on_leave(event, workButton, self.workTab))
+        self.workButton_window = self.canvas.create_window(90, 390, anchor=NW, window=workButton)
 
 def win():
     root = Tk()
--- P6/FORM/Energy/probSet.py
+++ P6/FORM/Energy/probSet.py
@@ -12,6 +12,13 @@ class ProblemSet:
     def on_mousewheel(self, event):
         self.canvas.yview_scroll(-1 * (event.delta // 120), "units")  # Scroll by units
     
+    
+    def on_enter(self, event, button, hover_image):
+        button.config(image=hover_image)
+
+    def on_leave(self, event, button, original_image):
+        button.config(image=original_image)
+ 
     def __init__(self, root):
         self.root = root
         self.root.geometry("1440x1024")
@@ -39,7 +46,12 @@ class ProblemSet:
             self.prob1 = PhotoImage(file="AssetsEnergy/probset1.png")
             self.prob2 = PhotoImage(file="AssetsEnergy/probset2.png")
             self.back = PhotoImage(file="UTILITY/backDash.png")
-            
+            self.powerTab = PhotoImage(file="UTILITY/powerr.png")
+            self.energyTab = PhotoImage(file="UTILITY/energyS.png")
+            self.workTab = PhotoImage(file="UTILITY/work.png")
+            self.powerHover = PhotoImage(file="UTILITY/powerS.png")
+            self.energyHover = PhotoImage(file="UTILITY/energyS.png")
+            self.workHover = PhotoImage(file="UTILITY/workS.png")
             # Create images on the canvas
             self.canvas_image = self.canvas.create_image(0, 0, anchor=NW, image=self.bg)
             self.tabID = self.canvas.create_image(52, 35, anchor=NW, image=self.tab)
@@ -61,7 +73,45 @@ class ProblemSet:
             command=self.controller.back_studyDash
         )
         self.canvas.create_window(414, 35, anchor=NW, window=backButton)
-        
+        powerButton = Button(
+            root, image=self.powerTab,
+            borderwidth=0,
+            background="#ffffff",
+            activebackground="#ffffff",
+            cursor="hand2",
+            # command=self.controller.open_problemSet
+        )
+        # Bind hover effects to masterButton
+        powerButton.bind("<Enter>", lambda event: self.on_enter(event, powerButton, self.powerHover))
+        powerButton.bind("<Leave>", lambda event: self.on_leave(event, powerButton, self.powerTab))
+        self.powerButton_window = self.canvas.create_window(90, 230, anchor=NW, window=powerButton)
+
+        energyButton = Button(
+            root, image=self.energyTab,
+            borderwidth=0,
+            background="#ffffff",
+            activebackground="#ffffff",
+            cursor="hand2",
+            # command=self.controller.open_problemSet
+        )
+        # Bind hover effects to masterButton
+        energyButton.bind("<Enter>", lambda event: self.on_enter(event, energyButton, self.energyHover))
+        energyButton.bind("<Leave>", lambda event: self.on_leave(event, energyButton, self.energyTab))
+        self.energyButton_window = self.canvas.create_window(90, 310, anchor=NW, window=energyButton)
+
+        workButton = Button(
+            root, image=self.workTab,
+            borderwidth=0,
+            background="#ffffff",
+            activebackground="#ffffff",
+            cursor="hand2",
+            # command=self.controller.open_problemSet
+        )
+        # Bind hover effects to masterButton
+        workButton.bind("<Enter>", lambda event: self.on_enter(event, workButton, self.workHover))
+        workButton.bind("<Leave>", lambda event: self.on_leave(event, workButton, self.workTab))
+        self.workButton_window = self.canvas.create_window(90, 390, anchor=NW, window=workButton)
+
 def win():
     root = Tk()
     ProblemSet(root)
